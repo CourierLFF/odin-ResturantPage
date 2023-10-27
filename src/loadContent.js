afc7ec64d336cf05f91d4e88fbbf93433780e257
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2023-10-27 00:39:50 -0400

    Let user cycle through foods

--- src/loadContent.js
+++ src/loadContent.js
@@ -21,59 +21,37 @@ export function loadContent() {
     container.appendChild(mainH3);
 
     const currentFood = document.createElement("div");
+    container.appendChild(currentFood);
+
+    function clearCurrentFood() {
+        while (currentFood.firstChild) {
+            currentFood.removeChild(currentFood.firstChild);
+        }
+    }
 
     const spaghettiButton = document.createElement("button");
     spaghettiButton.textContent = "Spaghetti";
+    spaghettiButton.addEventListener("click", () => {
+        clearCurrentFood();
+        currentFood.appendChild(showSpaghetti());
+    });
     container.appendChild(spaghettiButton);
 
     const cheeseburgerButton = document.createElement("button");
     cheeseburgerButton.textContent = "Cheeseburger";
+    cheeseburgerButton.addEventListener("click", () => {
+        clearCurrentFood();
+        currentFood.appendChild(showCheeseburger());
+    });
     container.appendChild(cheeseburgerButton);
 
     const steakButton = document.createElement("button");
     steakButton.textContent = "Steak";
+    steakButton.addEventListener("click", () => {
+        clearCurrentFood();
+        currentFood.appendChild(showSteak());
+    });
     container.appendChild(steakButton);
 
-    container.appendChild(showSpaghetti());
-
-
-
-    // const divElement1 = document.createElement("div");
-    // const spaghettiP = document.createElement("p");
-    // spaghettiP.textContent = "Spaghetti";
-    // const spaghettiPriceP = document.createElement("p");
-    // spaghettiPriceP.textContent = "$8.99";
-    // const spaghettiImg = document.createElement("img");
-    // spaghettiImg.src = "/src/assets/spaghetti.jpg";
-    // spaghettiImg.style = "width: 400px";
-    // divElement1.appendChild(spaghettiP);
-    // divElement1.appendChild(spaghettiPriceP);
-    // divElement1.appendChild(spaghettiImg);
-    // container.appendChild(divElement1);
-
-    // const divElement2 = document.createElement("div");
-    // const cheeseburgerP = document.createElement("p");
-    // cheeseburgerP.textContent = "Cheeseburger";
-    // const cheeseburgerPriceP = document.createElement("p");
-    // cheeseburgerPriceP.textContent = "$11.99";
-    // const cheeseburgerImg = document.createElement("img");
-    // cheeseburgerImg.src = "/src/assets/cheeseburger.jpg";
-    // cheeseburgerImg.style = "width: 400px";
-    // divElement2.appendChild(cheeseburgerP);
-    // divElement2.appendChild(cheeseburgerPriceP);
-    // divElement2.appendChild(cheeseburgerImg);
-    // container.appendChild(divElement2);
-
-    // const divElement3 = document.createElement("div");
-    // const steakP = document.createElement("p");
-    // steakP.textContent = "Steak";
-    // const steakPriceP = document.createElement("p");
-    // steakPriceP.textContent = "$13.99";
-    // const steakImg = document.createElement("img");
-    // steakImg.src = "/src/assets/steak.jpg";
-    // steakImg.style = "width: 400px";
-    // divElement3.appendChild(steakP);
-    // divElement3.appendChild(steakPriceP);
-    // divElement3.appendChild(steakImg);
-    // container.appendChild(divElement3);
+    container.appendChild(currentFood);
 }
